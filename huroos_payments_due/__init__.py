082c60adb51a4981038f62030665aafe1dbc3180
Michele-ati <110529983+Michele-ati@users.noreply.github.com>
2023-07-27 17:26:41 +0200

    Delete huroos_payments_due directory

--- huroos_payments_due/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Powered by Federico Ranieri.
-# Part of Huroos. See LICENSE file for full copyright and licensing details.
-# © 2022 Huroos Srl. (<https://www.huroos.com>).
-
-from . import models
--- huroos_payments_due/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Powered by Federico Ranieri.
-# Part of Huroos. See LICENSE file for full copyright and licensing details.
-# © 2022 Huroos Srl. (<https://www.huroos.com>).
-
-from . import payment_due_item
-from . import account_move
-from . import account_move_line
